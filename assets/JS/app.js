27db644ce3e55cc241c009962994fb9a8778891a
Datta Chavan <dattatraychavan192@gmail.com>
2026-06-23 19:03:11 +0530

    some work on UI

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -4,14 +4,16 @@ const todoForm = document.getElementById("todoForm");
 const title = document.getElementById("title");
 const completed = document.getElementById("completed");
 const userId = document.getElementById("userId");
-const addButton = document.getElementById("addButton");
-const updateButton = document.getElementById("updateButton");
+
 const cardContaier = document.getElementById("cardContaier");
 
-let baseURL = "https://jsonplaceholder.typicode.com";
+const addBtnn = document.getElementById("addbt");
+const updatBtn = document.getElementById("updatId");
 
 let TodoArr = [];
-const spinner = document.getElementById("spinner");
+let baseURl = "https://jsonplaceholder.typicode.com";
+
+let spinner = document.getElementById("spinner");
 
 function snackbar(msg, icon) {
   swal.fire({
@@ -21,116 +23,132 @@ function snackbar(msg, icon) {
   });
 }
 
-function makeApiCall(method, apiUrl, body = null, successCF, errorCF) {
+function todoStatus(status) {
+  if (status) {
+    return `<span class="badge bg-success">Completed</span>`;
+  } else {
+    return `<span class="badge bg-warning text-dark">Pending</span>`;
+  }
+}
+
+function makeApicall(methodName, apiUrl, body = null, successcb, errorcb) {
+  body = body ? JSON.stringify(body) : null;
+
   spinner.classList.remove("d-none");
 
-  body = body ? JSON.stringify(body) : null;
   let xhr = new XMLHttpRequest();
-  xhr.open(method, apiUrl);
+
+  xhr.open(methodName, apiUrl);
   xhr.setRequestHeader("content-type", "application/json");
   xhr.send(body);
   xhr.onload = function () {
     if (xhr.status >= 200 && xhr.status <= 299) {
       let res = JSON.parse(xhr.response);
-      if (method === "GET") {
-        successCF(res);
-      } else if (method === "POST") {
+      if (methodName === "GET") {
+        successcb(res);
+      } else if (methodName === "POST") {
         let obj = { ...JSON.parse(body), id: res.id };
-        successCF(obj);
-      } else if (method === "PATCH") {
-        successCF(JSON.parse(body));
+        successcb(obj);
+      } else if (methodName === "PUT") {
+        successcb(JSON.parse(body));
       } else {
-        successCF();
+        successcb();
       }
-      spinner.classList.add("d-none");
     } else {
-      snackbar("Something wents wgorng...");
+      snackbar("something wents wrong", "error");
     }
+    spinner.classList.add("d-none");
   };
 }
 
-makeApiCall("GET", `${baseURL}/todos`, null, creatPost, snackbar);
+makeApicall("GET", `${baseURl}/todos`, null, creatposts, snackbar);
 
-function creatPost(arr) {
+function creatposts(arr) {
   let result = "";
   arr.forEach((ele) => {
     result += `
-    <div class = "col-md-4 mt-4"  id="${ele.id}">
-          <div class="card shadow">
-            <div class="card-header  text-center text-light">
-              <h2>${ele.userId}</h2>
-            </div>
-            <div class="card-body ">
-              <p class="bg-light  text-center">${ele.completed}</p>
-              <p class="bg-light text-center">${ele.title}</p>
-            </div>
-            <div class="card-footer d-flex justify-content-between" >
-            <button class="btn   border-info" onclick="onEdit(this)"><i class="fa-solid fa-pen-to-square text-info"></i></button>
-            <button class="btn  border-danger" onclick="onDelete(this)"><i class="fa-solid text-danger fa-trash-can"></i></button>
-        </div>
+    <div class="col-md-3 mt-3"  id="${ele.id}">
+            <div class="card shadow">
+              <div class="card-header  text-center text-light">
+                <h2>${ele.userId}</h2>
+              </div>
+              <div class="card-body ">
+                <p class="  text-center">${todoStatus(ele.completed)}</p>
+                <p class="bg-light text-center">${ele.title}</p>
+              </div>
+              <div class="card-footer d-flex justify-content-between" >
+              <button class="btn   border-info" onclick="onEdit(this)"><i class="fa-solid fa-pen-to-square text-info"></i></button>
+              <button class="btn  border-danger" onclick="onDelete(this)"><i class="fa-solid text-danger fa-trash-can"></i></button>
           </div>
-          </div>
+            </div>
+      </div>
     `;
   });
+
   cardContaier.innerHTML = result;
 }
 
-function onEventHandalar(ele) {
+function onsubmitHandalar(ele) {
   ele.preventDefault();
 
-  let newObj = {
+  let obj = {
     title: title.value,
     completed: completed.value,
     userId: userId.value,
   };
 
-  let postUrl = `${baseURL}/todos`;
-  makeApiCall("POST", postUrl, newObj, creatSinglecart, snackbar);
+  let postUrl = `${baseURl}/todos`;
+
+  makeApicall("POST", postUrl, obj, creatsingleCard, snackbar);
 }
 
-function creatSinglecart(newObj) {
+function creatsingleCard(obj) {
   let div = document.createElement("div");
-  div.className = "col-md-4 mt-4";
-  div.id = newObj.id;
 
+  div.className = "col-md-3 mt-4";
+  div.id = obj.id;
   div.innerHTML = `
   <div class="card shadow">
-  <div class="card-header  text-center text-light">
-    <h2>${newObj.userId}</h2>
+      <div class="card-header  text-center text-light">
+      <h2>${obj.userId}</h2>
   </div>
-  <div class="card-body ">
-    <p class="bg-light  text-center">${newObj.completed}</p>
-    <p class="bg-light text-center">${newObj.title}</p>
-  </div>
-  <div class="card-footer d-flex justify-content-between" >
-  <button class="btn   border-info" onclick="onEdit(this)"><i class="fa-solid fa-pen-to-square text-info"></i></button>
-  <button class="btn  border-danger" onclick="onDelete(this)"><i class="fa-solid text-danger fa-trash-can"></i></button>
-</div>
-</div>
-  `;
-  cardContaier.prepend(div);
+    <div class="card-body ">
+      <p class="  text-center">${todoStatus(obj.completed)}</p>
+      <p class="bg-light text-center">${obj.title}</p>
+    </div>
 
-  snackbar(`New todo add with id ${newObj.id} successfully..`);
+     <div class="card-footer d-flex justify-content-between" >
+     <button class="btn   border-info" onclick="onEdit(this)"><i class="fa-solid fa-pen-to-square text-info"></i></button>
+      <button class="btn  border-danger" onclick="onDelete(this)"><i class="fa-solid text-danger fa-trash-can"></i></button>
+     </div>
+  </div>
+  `;
+
+  cardContaier.prepend(div);
+  todoForm.reset();
+  snackbar(`The post is added with id ${obj.id} Successfully`, "success");
 }
 
 function onEdit(ele) {
-  let editId = ele.closest(".col-md-4").id;
+  let editId = ele.closest(".col-md-3").id;
+
   localStorage.setItem("editId", editId);
+  let editUrl = `${baseURl}/todos/${editId}`;
 
-  let editUrl = `${baseURL}/todos/${editId}`;
-
-  makeApiCall("GET", editUrl, null, patchdata, snackbar);
+  makeApicall("GET", editUrl, null, patchData, snackbar);
 }
 
-function patchdata(res) {
+function patchData(res) {
   let editObj = res;
+
+  todoForm.classList.remove("d-none");
 
   title.value = editObj.title;
   completed.value = editObj.completed;
   userId.value = editObj.userId;
 
-  addButton.classList.add("d-none");
-  updateButton.classList.remove("d-none");
+  addBtnn.classList.add("d-none");
+  updatBtn.classList.remove("d-none");
 
   todoForm.scrollIntoView({
     behavior: "smooth",
@@ -138,32 +156,34 @@ function patchdata(res) {
   });
 }
 
-function updateCard(body) {
+function onupdateHandalar() {
   let updateId = localStorage.getItem("editId");
 
-  let updateObj = {
+  let updateUrl = `${baseURl}/todos/${updateId}`;
+
+  let updObj = {
     title: title.value,
     completed: completed.value,
     userId: userId.value,
+    id: updateId,
   };
 
-  let updateUrl = `${baseURL}/todos/${updateId}`;
+  makeApicall("PUT", updateUrl, updObj, updateOnUI, snackbar);
 
-  makeApiCall("PATCH", updateUrl, updateObj, changeUi, snackbar);
-  snackbar(`Todo update with id ${updateId} successfully..`);
+  snackbar(`Post Updated with id ${updateId} Successfully`, "success");
 }
 
-function changeUi(body) {
+function updateOnUI(body) {
   let updateId = localStorage.getItem("editId");
   let div = document.getElementById(updateId);
-  div.className = "col-md-4 mt-4";
+  div.className = "col-md-3 mt-4";
   div.innerHTML = `
   <div class="card shadow">
-  <div class="card-header  text-center text-light">
+  <div class="card-header  text-center text-light" >
     <h2>${body.userId}</h2>
   </div>
   <div class="card-body ">
-    <p class="bg-light  text-center">${body.completed}</p>
+    <p class="  text-center">${todoStatus(body.completed)}</p>
     <p class="bg-light text-center">${body.title}</p>
   </div>
   <div class="card-footer d-flex justify-content-between" >
@@ -171,11 +191,11 @@ function changeUi(body) {
   <button class="btn  border-danger" onclick="onDelete(this)"><i class="fa-solid text-danger fa-trash-can"></i></button>
 </div>
 </div>
+  
   `;
 
-  addButton.classList.remove("d-none");
-  updateButton.classList.add("d-none");
-
+  addBtnn.classList.remove("d-none");
+  updatBtn.classList.add("d-none");
   todoForm.reset();
 
   div.scrollIntoView({
@@ -185,31 +205,34 @@ function changeUi(body) {
 }
 
 function onDelete(ele) {
+  let removeId = ele.closest(".col-md-3").id;
+
   Swal.fire({
     title: "Are you sure?",
-    text: "You want to delete it!",
+    text: "You want to delete it ",
     icon: "warning",
     showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
+    confirmButtonColor: "#2e86f2",
+    cancelButtonColor: "rgb(193, 19, 19)",
     confirmButtonText: "Yes, delete it!",
   }).then((result) => {
     if (result.isConfirmed) {
-      let deleteId = ele.closest(".col-md-4").id;
-      localStorage.setItem("deleteId", deleteId);
-      let deleteUrl = `${baseURL}/todos/${deleteId}`;
+      localStorage.setItem("removeId", removeId);
 
-      makeApiCall("DELETE", deleteUrl, null, removeCard, snackbar);
-      snackbar(`Todo delete with id ${deleteId} successfully..`);
+      let removeURL = `${baseURl}/todos/${removeId}`;
+
+      makeApicall("DELETE", removeURL, null, onDeleteFun, snackbar);
+    } else {
+      snackbar("you canceled it ");
     }
   });
 }
 
-function removeCard() {
-  let deleteId = localStorage.getItem("deleteId");
+function onDeleteFun() {
+  let deleteId = localStorage.getItem("removeId");
   document.getElementById(deleteId).remove();
+  snackbar(`Card delete with id ${deleteId} successfully`, "success");
 }
 
-todoForm.addEventListener("submit", onEventHandalar);
-
-updateButton.addEventListener("click", updateCard);
+todoForm.addEventListener("submit", onsubmitHandalar);
+updatBtn.addEventListener("click", onupdateHandalar);
